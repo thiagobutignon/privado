e59ebe03d7c33c1ab1b86facdf87e019da7d675b
Thiago Butignon Claramunt <thiagobutignon@MacBook-Pro-de-Thiago.local>
2017-10-18 02:43:00 -0200

    first commit

---
+++ screens/RechargeScreen.js
@@ -0,0 +1,147 @@
+import React from 'react';
+import {  
+  Image,
+  Platform,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import { WebBrowser, BarCodeScanner, Permissions } from 'expo';
+
+import { MonoText } from '../components/StyledText';
+
+export default class RechargeScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Recharge',
+    header: null,
+  };
+
+  state = {
+      hasCameraPermission: null,
+  }
+  componentDidMount() {
+      this._requestCameraPermission();
+  }
+  
+  _requestCameraPermission = async () => {
+      const { status } = await Permissions.askAsync(Permissions.CAMERA);
+      this.setState({
+          hasCameraPermission: status === 'granted',
+      });
+    };
+
+    _handleBarCodeRead = ({data}) => {
+        WebBrowser.openBrowserAsync(
+            JSON.stringify({data})
+        );
+    };
+
+render() {
+    return (
+      <View style={styles.container}>
+        {this.state.hasCameraPermission === null ?
+          <Text>Requesting for camera permission</Text> :
+          this.state.hasCameraPermission === false ?
+            <Text>Camera permission is not granted</Text> :
+            <BarCodeScanner
+              onBarCodeRead={this._handleBarCodeRead}
+              style={{ height: 200, width: 200 }}
+            />
+        }
+      </View>
+    );
+  }
+}
+
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+    },
+    developmentModeText: {
+      marginBottom: 21,
+      color: 'rgba(0,0,0,0.4)',
+      fontSize: 14,
+      lineHeight: 19,
+      textAlign: 'center',
+    },
+    contentContainer: {
+      paddingTop: 30,
+    },
+    welcomeContainer: {
+      alignItems: 'center',
+      marginTop: 10,
+      marginBottom: 20,
+    },
+    welcomeImage: {
+      width: 100,
+      height: 80,
+      resizeMode: 'contain',
+      marginTop: 3,
+      marginLeft: -10,
+    },
+    getStartedContainer: {
+      alignItems: 'center',
+      marginHorizontal: 50,
+    },
+    homeScreenFilename: {
+      marginVertical: 7,
+    },
+    codeHighlightText: {
+      color: 'rgba(96,100,109, 0.8)',
+    },
+    codeHighlightContainer: {
+      backgroundColor: 'rgba(0,0,0,0.05)',
+      borderRadius: 3,
+      paddingHorizontal: 4,
+    },
+    getStartedText: {
+      fontSize: 17,
+      color: 'rgba(96,100,109, 1)',
+      lineHeight: 24,
+      textAlign: 'center',
+    },
+    tabBarInfoContainer: {
+      position: 'absolute',
+      bottom: 0,
+      left: 0,
+      right: 0,
+      ...Platform.select({
+        ios: {
+          shadowColor: 'black',
+          shadowOffset: { height: -3 },
+          shadowOpacity: 0.1,
+          shadowRadius: 3,
+        },
+        android: {
+          elevation: 20,
+        },
+      }),
+      alignItems: 'center',
+      backgroundColor: '#fbfbfb',
+      paddingVertical: 20,
+    },
+    tabBarInfoText: {
+      fontSize: 17,
+      color: 'rgba(96,100,109, 1)',
+      textAlign: 'center',
+    },
+    navigationFilename: {
+      marginTop: 5,
+    },
+    helpContainer: {
+      marginTop: 15,
+      alignItems: 'center',
+    },
+    helpLink: {
+      paddingVertical: 15,
+    },
+    helpLinkText: {
+      fontSize: 14,
+      color: '#2e78b7',
+    },
+  });
+  
